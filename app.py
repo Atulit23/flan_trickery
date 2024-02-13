acefa20bf00a09046b5d9e70cc7e509a638296dc
atulit gaur <atulit23@gmail.com>
2024-02-14 03:44:16 +0530

    added direct gradio support instead of bridging via nodejs

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 import requests
 from flask_cors import CORS
+from gradio_client import Client
 
+client = Client("https://atulit23-google-flan-t5.hf.space/")
 unified_url = "https://review-and-ads.vercel.app/text-interference"
 app = Flask(__name__)
 CORS(app)  
@@ -33,7 +35,7 @@ def termsOfCancellation():
 
         if len(indices) > 1:
             if (indices[-1] - indices[0] > 600):
-                new_text = ' '.join(all_text[indices[0] + 100: indices[-1] + 100])
+                new_text = ' '.join(all_text[indices[0] + 200: indices[-1] + 100])
                 # if (indices[-1] - indices[-2] > 600):
                 #     new_text = ' '.join(all_text[indices[-1] - 120: indices[-1] + 120])
                 # else:
@@ -50,15 +52,21 @@ def termsOfCancellation():
         data = {'text': new_text}
 
         try:
-            response = requests.post(unified_url, json=data)
-            response.raise_for_status()
-            print(response.text)
-            if(response.text.find("no") != -1):
-                return jsonify({"response": "The terms of cancellation are not clear."})
+            result = client.predict(
+				new_text,
+				api_name="/predict"
+            )
+            print(result)
+            # response = requests.post(unified_url, json=data)
+            # response.raise_for_status()
+            # print(response.text)
+            if(result.find("no") != -1):
+                return jsonify({"response": "They don't explain you how to cancel your subscription!"})
             else:
-                return jsonify({"response": "The terms of cancellation are clear."})
-        except:
-            return jsonify({"response": "Error occured"})
+                return jsonify({"response": "They explain you how to cancel your subscription!"})
+        except Exception as e:
+            print(f"Error occurred: {str(e)}")
+            return jsonify({"response": f"Error occurred: {str(e)}"})
 
     else:
         return jsonify({'error': f"Failed to retrieve the webpage. Status code: {response.status_code}"}), 500
@@ -89,7 +97,7 @@ def howToCancel():
         print(indices)
         if len(indices) > 1:
             if (indices[-1] - indices[0] > 600):
-                new_text = ' '.join(all_text[indices[0] + 100: indices[-1] + 100])
+                new_text = ' '.join(all_text[indices[0] + 200: indices[-1] + 100])
                 # if (indices[-1] - indices[-2] > 600):
                 #     new_text = ' '.join(all_text[indices[-1] - 120: indices[-1] + 120])
                 # else:
@@ -100,20 +108,26 @@ def howToCancel():
             new_text = ' '.join(all_text[indices[0] - 120: indices[0] + 150])
 
         new_text = new_text + '. Do they tell you how to cancel the subscription? Answer in yes or no.'
-        print(new_text)
-        
+        # print(new_text)
+
         data = {'text': new_text}
 
         try:
-            response = requests.post(unified_url, json=data)
-            response.raise_for_status()
-            print(response.text)
-            if(response.text.find("no") != -1):
+            result = client.predict(
+				new_text,
+				api_name="/predict"
+            )
+            print(result)
+            # response = requests.post(unified_url, json=data)
+            # response.raise_for_status()
+            # print(response.text)
+            if(result.find("no") != -1):
                 return jsonify({"response": "They don't explain you how to cancel your subscription!"})
             else:
                 return jsonify({"response": "They explain you how to cancel your subscription!"})
-        except:
-            return jsonify({"response": "Error occured"})
+        except Exception as e:
+            print(f"Error occurred: {str(e)}")
+            return jsonify({"response": f"Error occurred: {str(e)}"})
     
     else:
         return jsonify({'error': f"Failed to retrieve the webpage. Status code: {response.status_code}"}), 500
